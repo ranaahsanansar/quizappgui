f99f66206d0a6f0083a301e448bf9bd6acc5573b
Rana Ahsan Ansar <asn.cs21@gmail.com>
2022-05-25 07:06:54 -0700

    Crete Student Home View and Controller

--- src/main/java/ahsan/quizapplication/Controllers/StudetnHomeController.java
+++ src/main/java/ahsan/quizapplication/Controllers/StudetnHomeController.java
@@ -1,18 +1,17 @@
 package ahsan.quizapplication.Controllers;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class StudentHomeController implements Initializable {
-    @FXML
-    private Button backBtn;
-    @FXML
-    private StackPane stackPane;
+public class StudetnHomeController implements Initializable {
+    public Button backbtn;
+    public StackPane stackPane;
+    public FlowPane flowPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
